562c9bc13e0fb81e157cc9a4743185ebcc883b6f
punytan <punytan@gmail.com>
2011-01-06 18:03:11 +0900

    implemented

--- lib/practical.pm
+++ lib/practical.pm
@@ -1,28 +1,68 @@
 package practical;
-use strict;
-use warnings;
 our $VERSION = '0.01';
 
+sub import {
+    require strict;
+    strict->import;
+
+    require warnings;
+    warnings->import;
+
+    require utf8;
+    utf8->import;
+
+    require feature;
+    feature->import('switch', 'say', 'state');
+}
 1;
+
 __END__
 
 =head1 NAME
 
-practical -
+practical - practical pragma
 
 =head1 SYNOPSIS
 
   use practical;
 
+  # is the same as:
+
+  use strict;
+  use warnings;
+  use utf8;
+  use feature qw(switch say state);
+
 =head1 DESCRIPTION
 
-practical is
+C<practical> is a practical pragma.
+
+This module is written for people who are tired of typing hackneyed expressions.
+Many people write following code on the top of scripts.
+
+  use strict;
+  use warnings;
+  use utf8;
+  use feature qw(switch say state);
+
+Assuming that it is true, what we should do about it is shorten it.
+
+=head1 THERE IS NO C<no practical>!
+
+Did you write many C<no> like below?
+
+  no strict;
+  no warnings;
+  no utf8;
+  no feature;
 
 =head1 AUTHOR
 
 punytan E<lt>punytan@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<Modern::Perl>, L<common::sense>
 
 =head1 LICENSE
 
